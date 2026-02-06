02d7a40013a7fe31b0d95b411cd63df673f79a4f
Ricardo <ricardo_goncalves2525@hotmail.com>
2026-02-06 14:59:37 +0000

    display social media true false

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -16,22 +16,24 @@ const IconWrapper = styled.span`
 export default function socialMedia(props) {
   return (
     <div className="social-media-div">
-      {socialMediaLinks.map((media, i) => {
-        return (
-          <a
-            key={i}
-            href={media.link}
-            className={`icon-button`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <IconWrapper {...media} {...props}>
-              <i className={`fab ${media.fontAwesomeIcon}`}></i>
-            </IconWrapper>
-            {/* <span></span> */}
-          </a>
-        );
-      })}
+      {socialMediaLinks
+        .filter((media) => media.display !== false)
+        .map((media, i) => {
+          return (
+            <a
+              key={i}
+              href={media.link}
+              className={`icon-button`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconWrapper {...media} {...props}>
+                <i className={`fab ${media.fontAwesomeIcon}`}></i>
+              </IconWrapper>
+              {/* <span></span> */}
+            </a>
+          );
+        })}
     </div>
   );
 }
